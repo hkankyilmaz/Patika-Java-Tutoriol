eda43eb23367a4dae919b1d91474e9018559113c
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2023-03-07 22:26:43 +0300

    average Notes class

---
+++ NotesAverage.java
@@ -0,0 +1,28 @@
+    import java.util.Scanner;
+
+    public class NotesAverage {
+        public static void main(String[] args) {
+            double math , biology , history , chemistiry , geograpy ;
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Please enter yours Math note : ");
+            math = input.nextInt();
+
+            System.out.print("Please enter yours Biology note : ");
+            biology = input.nextInt();
+
+            System.out.print("Please enter yours History note : ");
+            history = input.nextInt();
+
+            System.out.print("Please enter yours Chemistiry note : ");
+            chemistiry = input.nextInt();
+
+            System.out.print("Please enter yours Geograpy note : ");
+            geograpy = input.nextInt();
+
+            double averageNotes = ( biology + math + history + chemistiry + geograpy  ) / 5 ;
+
+            System.out.println("Your Average Note is : " + averageNotes);
+
+        }
+    }
