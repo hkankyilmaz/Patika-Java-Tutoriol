311b23c9fa5d13afc922af0970955fd137c112fa
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2023-03-08 11:59:37 +0300

    CalculateKDV class completed

--- CalculateKDV.java
+++ CalculateKDV.java
@@ -3,6 +3,26 @@
         public class CalculateKDV {
             public static void main(String[] args) {
                 Scanner input = new Scanner(System.in);
-                double input = 
+
+                System.out.print("Please enter the amount of the money : ");
+                double amountOfMoney = input.nextDouble(),withKdv8 = 0,withKdv18= 0;
+
+                boolean isNumberGreater = amountOfMoney > 100;
+
+                if (isNumberGreater) {
+                     withKdv8 = amountOfMoney + (amountOfMoney * 0.08);
+                }else {
+                    withKdv18 = amountOfMoney + (amountOfMoney * 0.18);
+                }
+
+                System.out.println("Principal : " + amountOfMoney );
+                if (isNumberGreater) {
+                    System.out.println("Price--with KDV-- : " + withKdv8 );
+                    System.out.println("KDV Cost : " + (withKdv8 - amountOfMoney) );
+                }else {
+                    System.out.println("Price--with KDV-- : " + withKdv18 );
+                    System.out.println("KDV Cost : " + (withKdv18 - amountOfMoney) );
+                }
+
             }
         }
--- README.md
+++
@@ -1,2 +0,0 @@
-# Patika-Java-Tutoriol
- 
